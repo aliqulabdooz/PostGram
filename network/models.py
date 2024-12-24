12d8e49810dbfb0565ab93d66946edfad1cb49b9
aliqulabdooz <aliqulabdooz@gmail.com>
2024-12-24 13:23:27 +0330

    add postcomment, postlike and postarchive in network models

--- network/models.py
+++ network/models.py
@@ -27,3 +27,30 @@ class Follower(models.Model):
 
     def __str__(self):
         return self.follow.username
+
+
+class PostLike(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_likes')
+    like = models.ManyToManyField(Post)
+
+    def __str__(self):
+        return self.user.username
+
+
+class PostArchive(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_archive')
+    archive = models.ManyToManyField(Post)
+
+    def __str__(self):
+        return self.user.username
+
+
+class PostComment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_commented')
+    post = models.ManyToManyField(Post)
+    comment = models.TextField(blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
